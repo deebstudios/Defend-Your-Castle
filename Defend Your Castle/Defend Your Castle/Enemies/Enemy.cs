1bd3eb4a6e7cd27efb5a39b79cd920ee1c4405fb
adeeb1 <adeeb1@babson.edu>
2014-09-12 20:27:49 -0400

    Created the Enemy class
    -Started setting up enemies

---
+++ Defend Your Castle/Defend Your Castle/Enemies/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Defend_Your_Castle
+{
+    public class Enemy : LevelObject
+    {
+        // Movement speed
+        private Vector2 MoveSpeed;
+
+        // Range of the Enemy. Use 1 for melee enemies
+        private int Range;
+
+        // The point on the level at which the enemy will stop moving
+        private Vector2 StopPoint;
+
+        public Enemy()
+        {
+            // Set the enemy's properties
+            MoveSpeed = new Vector2(10, 0);
+            Range = 1;
+
+            //StopPoint = (CastleX - Range);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
+
+            base.Draw(spriteBatch);
+        }
+
+    }
+}
