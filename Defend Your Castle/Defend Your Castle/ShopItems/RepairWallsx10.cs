44a039d672039d5c612d931d09ac012c4305a726
tdeeb <thomasdeeb1@gmail.com>
2014-10-22 23:41:40 -0400

    Added the remaining sounds
    -Added a new Goblin Death sound that isn't as harsh
    -Added a Level Complete sound
    -Lowered the volume of the Weapon Swing sound
    -Removed the SOR2 sound effect, aka "test"
    -Played the sounds at the proper times
    
    -Created a new ShopItem, RepairWallsx10, which will also be our final one
    -Created a background for the Title Screen

---
+++ Defend Your Castle/Defend Your Castle/ShopItems/RepairWallsx10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defend_Your_Castle.ShopItems
+{
+    public sealed class RepairWallsx10 : RepairWalls
+    {
+        public RepairWallsx10(Player shopPlayer, Shop shop) : base(shopPlayer, shop)
+        {
+            HealAmount = 10000;
+
+            Description = "Greatly repair your fort's walls.\n+" + HealAmount + " Health";
+
+            // Get the path to the image of the item
+            ImagePath = "Content/Graphics/ShopIcons/Big RepairWallsx10Icon.png";
+        }
+    }
+}
