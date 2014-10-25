1dbb2b21e993b32ef6bc54857a3770d985cf5b08
adeeb1@babson.edu <adeeb1@babson.edu>
2014-10-25 19:07:09 -0400

    Started Creating the New How To Play Screen
    - It looks just like the old How To Play Screen but consists of a single HUD grid different with elements being shown/hidden
    - Created the left and right arrow buttons for the screen
    - Added the HTP_ScreenNum variable to ScreenManager.cs for keeping track of which How To Play screen the player is viewing

--- Defend Your Castle/Defend Your Castle/ScreenManager.cs
+++ Defend Your Castle/Defend Your Castle/ScreenManager.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Microsoft.Xna.Framework;
 
 namespace Defend_Your_Castle
 {
@@ -16,6 +18,9 @@ namespace Defend_Your_Castle
         // References to Game1.cs and GamePage.cs
         public Game1 Game;
         public GamePage GamePage;
+
+        // Stores the screen number for the How To Play Screen
+        public int HTP_ScreenNum = 1;
 
         public ScreenManager(Game1 game, GamePage gamePage)
         {
@@ -75,6 +80,10 @@ namespace Defend_Your_Castle
 
             // GameOver Screen
             GamePage.GameOverScreen_Continue.Click += GameOverScreen_Continue;
+
+            // How To Play Screen
+            GamePage.HTP_LeftArrowButton.Click += HowToPlayScreen_ChangeScreens;
+            GamePage.HTP_RightArrowButton.Click += HowToPlayScreen_ChangeScreens;
         }
 
         // Title Screen
@@ -125,6 +134,71 @@ namespace Defend_Your_Castle
             ChangeScreen(Screens.TitleScreen);
         }
 
+        // How To Play Screen
+
+        private void HowToPlayScreen_ChangeScreens(object sender, RoutedEventArgs e)
+        {
+            // Get the Button that was clicked
+            Button btn = (Button)sender;
+
+            // Hide the current screen's elements
+            ChangeHowToPlayScreen(Visibility.Collapsed);
+
+            // Increase the screen number if the right arrow button was clicked; otherwise, decrease the screen number
+            HTP_ScreenNum += ((btn == GamePage.HTP_RightArrowButton) ? 1 : -1);
+
+            // Make sure the screen number doesn't go below 1
+            if (HTP_ScreenNum < 1) HTP_ScreenNum = 1;
+
+            // Hide the left arrow button if the screen number is 1
+            GamePage.HTP_LeftArrowButton.Visibility = ((HTP_ScreenNum == 1) ? Visibility.Collapsed : Visibility.Visible);
+
+            // Show the current screen's elements
+            ChangeHowToPlayScreen(Visibility.Visible);
+        }
+
+        // Changes the display of the How To Play Screen based on the screen number the player is viewing
+        private void ChangeHowToPlayScreen(Visibility visibility)
+        {
+            switch (HTP_ScreenNum)
+            {
+                case 1: // Intro
+                    GamePage.HTP_HUD_Screen1_Intro.Visibility = visibility;
+
+                    break;
+                case 2: // Goblin
+                    // Check if the screen is being shown
+                    if (visibility == Visibility.Visible)
+                    {
+                        // Create a new MeleeEnemy
+                        MeleeEnemy goblin = new MeleeEnemy(Game.level, Game1.ScreenHalf.Y, 0, 1);
+
+                        // Move the goblin in plain view
+                        goblin.Move(new Vector2(150, 50));
+
+                        // Add the goblin to the level
+                        Game.level.AddEnemy(goblin);
+                    }
+                    else // The screen is being hidden
+                    {
+                        // Remove all enemies from the level
+                        Game.level.GetEnemies.Clear();
+                    }
+
+                    break;
+                case 3: // Weapons
+                    GamePage.HTP_HUD_WeaponButtonColumn.Visibility = visibility;
+                    GamePage.HTP_HUD_Screen3_WeaponInfo.Visibility = visibility;
+                    GamePage.HTP_HUD_HealthGoldColumn.Visibility = visibility;
+                    GamePage.HTP_HUD_PauseColumn.Visibility = visibility;
+                    
+                    // Add a border thickness of 3 if the screen is being shown
+                    GamePage.HTP_HUD_Screen3_RedBox.BorderThickness = ((visibility == Visibility.Visible) ?
+                                                                       new Thickness(3) : new Thickness(0));
+
+                    break;
+            }
+        }
 
     }
 }
