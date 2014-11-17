a683d23fcb3a540fc0dfc18609982ce4782ee136
adeeb1@babson.edu <adeeb1@babson.edu>
2014-11-16 19:40:17 -0500

    Updated the Game's Build and Package Settings
    -Changed the assembly name, package version number, publisher display name, product name, company name, and copyright information

--- Defend Your Castle/Defend Your Castle/Properties/AssemblyInfo.cs
+++ Defend Your Castle/Defend Your Castle/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Defend Your Castle")]
-[assembly: AssemblyProduct("Defend Your Castle")]
+[assembly: AssemblyTitle("Defend Your Fort")]
+[assembly: AssemblyProduct("Defend Your Fort")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Deeb Studios")]
+[assembly: AssemblyCopyright("Copyright © 2014 Deeb Studios")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,3 +32,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
